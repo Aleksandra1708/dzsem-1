8e71029c837a0756482403f767e920fa643980fe
Alaksandra Pavlova <wellessa1717@gmail.com>
2023-04-11 22:59:21 +0300

    задача 8

---
+++ dzsem8zadacha/Program.cs
@@ -0,0 +1,13 @@
+﻿         Console.Write("ВВедите первое число: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("ВВедите втрое число: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        for (int i = a; i <= b; i++)
+        {
+            if (i % 2 == 0) 
+            {
+                Console.Write(i + " ");
+            }
+        }
+         
+        
