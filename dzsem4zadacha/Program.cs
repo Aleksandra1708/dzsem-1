885a6ba1b45dc644dd6ddf8cb830aa3a7dcb775c
Alaksandra Pavlova <wellessa1717@gmail.com>
2023-04-11 21:53:19 +0300

    задача 4/1

--- dzsem4zadacha/Program.cs
+++ dzsem4zadacha/Program.cs
@@ -1,2 +1,26 @@
-﻿int Max(int a, int b) => a > b ? a : b;
-int Max(int a, int b, int c) => Max(Max(a, b), c);
+﻿ Console.WriteLine("Введи 3 числа и мы тебе скажем какое из них максимально");
+            int a, b, c;
+            Console.WriteLine("Введи свое первое число - ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введи свое второе число - ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введи свое третье число - ");
+            c= Convert.ToInt32(Console.ReadLine());
+ 
+
+           if (a > b)
+           {
+            Console.WriteLine($"Число {b} максимально");
+           }
+           else if (b < c)
+           {
+            Console.WriteLine($"Число {c} максимально");
+           }
+           else if (c < b)
+            {
+            Console.WriteLine($"Число {b} максимально");
+           }
+           else if (a > c)
+            {
+            Console.WriteLine($"Число {a} максимально");
+           }
