e6331e8faefa245930b599481708c24d66924dae
Alaksandra Pavlova <wellessa1717@gmail.com>
2023-04-11 22:43:22 +0300

    задача 6

---
+++ dzsem6zadacha/Program.cs
@@ -0,0 +1,11 @@
+﻿        Console.WriteLine("Введи число и мы тебе скажем является ли число чётным (делится ли оно на два без остатка)"); 
+        int a = Convert.ToInt32(Console.ReadLine());
+                
+        if (a%2==0)    
+        {
+            Console.WriteLine($"Число {a} чётное");
+        }
+         else 
+         {
+            Console.WriteLine($"Число {a} нечётное");
+         }
